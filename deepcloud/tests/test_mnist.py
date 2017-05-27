d6d2185ed0379da2ae62f1367e9b6b84bf48b764
Roni <roni723@gmail.com>
2017-05-27 14:11:25 +0200

    first tests

---
+++ deepcloud/tests/test_mnist.py
@@ -0,0 +1,86 @@
+from __future__ import print_function
+from keras.applications.resnet50 import ResNet50
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+from keras.utils import plot_model
+import numpy as np
+import pydot
+import graphviz
+
+
+# In[3]:
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+
+# In[4]:
+
+img_rows, img_cols = 28, 28
+
+
+# In[5]:
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, 1)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+
+# In[6]:
+
+'''print('x_train shape:', x_train.shape)
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')'''
+
+
+# In[7]:
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+
+# In[8]:
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+
+# In[9]:
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+metrics=['accuracy'])
+
+
+# In[ ]:
+
+plot_model(model, to_file='model.png')
+
+
+# In[32]:
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=2,
+          validation_data=(x_test, y_test))
+
