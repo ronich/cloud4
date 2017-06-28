d03d4ccc005b9f7beb3bc2ad2c90fb21c232577a
Roni <roni723@gmail.com>
2017-06-28 22:38:53 +0200

    charts with cost, comparison tables

--- deepcloud/analysis/exploratory.py
+++ deepcloud/analysis/exploratory.py
@@ -82,88 +82,123 @@ get_ipython().magic('matplotlib inline')
 
 tmp = df_all
 tmp['timecum'] = df_all.sort_values(by='epoch').groupby(['data_arch','instance_type']).cumsum()['time']
+tmp.loc[tmp['instance_type'] == 'c4.2xlarge', 'price_ond'] = tmp['timecum']*0.3980/3600
+tmp.loc[tmp['instance_type'] == 'p2.xlarge', 'price_ond'] = tmp['timecum']*0.9000/3600
+tmp.loc[tmp['instance_type'] == 'c4.2xlarge', 'price_spot'] = tmp['timecum']*0.1092/3600
+tmp.loc[tmp['instance_type'] == 'p2.xlarge', 'price_spot'] = tmp['timecum']*0.2450/3600
 df_grp = tmp.groupby('data_arch')
 
 lsd = {'c4.2xlarge':'solid', 'p2.xlarge':'dashed'}
 
-fig, axes = plt.subplots(nrows=3, ncols=2, figsize=[14, 18], sharey=False)
-fig.subplots_adjust(hspace=0.35,wspace=0.25)
+for x in ['timecum','price_ond','price_spot']:
+    fig, axes = plt.subplots(nrows=3, ncols=2, figsize=[14, 18], sharey=False)
+    fig.subplots_adjust(hspace=0.35,wspace=0.25)
+    for i, k in enumerate(sorted(list(df_grp.groups.keys()))):
+        group = df_grp.get_group(k)
+        labels=[]
+        for key, grp in group.groupby(['instance_type']):
+            ax = grp.plot(ax=axes[floor(i/2)][i%2], kind='line', x=x, y='loss', c='black', ls=lsd.get(key))
+            labels.append(key)
+            lines, _ = ax.get_legend_handles_labels()
+            ax.legend(lines, labels, loc=1)
+            if i == 0:
+                start, end = ax.get_xlim()
+                ax.xaxis.set_ticks(range(0, int(end)+1, 10000))
+        axes[floor(i/2)][i%2].set_title('Zbiór danych: {}\nArchitektura: {}'.format(k.split('_')[0], k.split('_')[1]))
+        if x == 'timecum':
+            axes[floor(i/2)][i%2].set_xlabel('Czas obliczeń (s)')
+        elif x == 'price_ond':
+            axes[floor(i/2)][i%2].set_xlabel('Cena obliczeń on-demand ($)')
+        elif x == 'price_spot':
+            axes[floor(i/2)][i%2].set_xlabel('Cena obliczeń spot ($)')
+        axes[floor(i/2)][i%2].set_ylabel('Wartość funkcji celu')
 
-for i, k in enumerate(sorted(list(df_grp.groups.keys()))):
-    group = df_grp.get_group(k)
-    labels=[]
-    for key, grp in group.groupby(['instance_type']):
-        ax = grp.plot(ax=axes[floor(i/2)][i%2], kind='line', x='timecum', y='loss', c='black', ls=lsd.get(key))
-        labels.append(key)
+    fig.suptitle('')
+    fig.savefig('fig:experiment_loss_{}.png'.format(x))
+
+for x in ['timecum','price_ond','price_spot']:
+    fig, axes = plt.subplots(nrows=3, ncols=2, figsize=[14, 18], sharey=False)
+    fig.subplots_adjust(hspace=0.35, wspace=0.25)
+
+    for i, k in enumerate(sorted(list(df_grp.groups.keys()))):
+        group = df_grp.get_group(k)
+        labels=[]
+        for key, grp in group.groupby(['instance_type']):
+            ax = grp.plot(ax=axes[floor(i/2)][i%2], kind='line', x=x, y='acc', c='black', ls=lsd.get(key))
+            labels.append(key)
+            lines, _ = ax.get_legend_handles_labels()
+            ax.legend(lines, labels, loc=4)
+            if i == 0:
+                start, end = ax.get_xlim()
+                ax.xaxis.set_ticks(range(0, int(end)+1, 10000))
+        axes[floor(i/2)][i%2].set_title('Zbiór danych: {}\nArchitektura: {}'.format(k.split('_')[0], k.split('_')[1]))
+        if x == 'timecum':
+            axes[floor(i/2)][i%2].set_xlabel('Czas obliczeń (s)')
+        elif x == 'price_ond':
+            axes[floor(i/2)][i%2].set_xlabel('Cena obliczeń on-demand ($)')
+        elif x == 'price_spot':
+            axes[floor(i/2)][i%2].set_xlabel('Cena obliczeń spot ($)')
+        axes[floor(i/2)][i%2].set_ylabel('Wartość funkcji celu')
+        axes[floor(i/2)][i%2].set_ylabel('Trafność')
+
+    fig.suptitle('')
+    fig.savefig('fig:experiment_acc_{}.png'.format(x))
+
+for x in ['timecum','price_ond','price_spot']:
+    fig, axes = plt.subplots(nrows=3, ncols=2, figsize=[14, 18], sharey=False)
+    fig.subplots_adjust(hspace=0.35,wspace=0.25)
+
+    for i, k in enumerate(sorted(list(df_grp.groups.keys()))):
+        group = df_grp.get_group(k)
+        for key, grp in group.groupby(['instance_type']):
+            ax = grp.plot(ax=axes[floor(i/2)][i%2], kind='line', x=x, y='val_loss', c='black', ls=lsd.get(key))
+            labels.append(key)
+            if i == 0:
+                start, end = ax.get_xlim()
+                ax.xaxis.set_ticks(range(0, int(end)+1, 10000))
         lines, _ = ax.get_legend_handles_labels()
-        ax.legend(lines, labels, loc=1)
-        if i == 0:
-            start, end = ax.get_xlim()
-            ax.xaxis.set_ticks(range(0, int(end)+1, 10000))
-    axes[floor(i/2)][i%2].set_title('Zbiór danych: {}\nArchitektura: {}'.format(k.split('_')[0], k.split('_')[1]))
-    axes[floor(i/2)][i%2].set_xlabel('Czas obliczeń (s)')
-    axes[floor(i/2)][i%2].set_ylabel('Wartość funkcji celu')
+        ax.legend(lines, labels, loc='best')
+        axes[floor(i/2)][i%2].set_title('Zbiór danych: {}\nArchitektura: {}'.format(k.split('_')[0], k.split('_')[1]))
+        if x == 'timecum':
+            axes[floor(i/2)][i%2].set_xlabel('Czas obliczeń (s)')
+        elif x == 'price_ond':
+            axes[floor(i/2)][i%2].set_xlabel('Cena obliczeń on-demand ($)')
+        elif x == 'price_spot':
+            axes[floor(i/2)][i%2].set_xlabel('Cena obliczeń spot ($)')
+        axes[floor(i/2)][i%2].set_ylabel('Wartość funkcji celu')
 
-fig.suptitle('')
-fig.savefig('fig:experiment_loss.png')
+    fig.suptitle('')
+    fig.savefig('fig:experiment_loss_val_{}.png'.format(x))
 
-fig, axes = plt.subplots(nrows=3, ncols=2, figsize=[14, 18], sharey=False)
-fig.subplots_adjust(hspace=0.35, wspace=0.25)
+for x in ['timecum','price_ond','price_spot']:
+    fig, axes = plt.subplots(nrows=3, ncols=2, figsize=[14, 18], sharey=False)
+    fig.subplots_adjust(hspace=0.35,wspace=0.25)
 
-for i, k in enumerate(sorted(list(df_grp.groups.keys()))):
-    group = df_grp.get_group(k)
-    labels=[]
-    for key, grp in group.groupby(['instance_type']):
-        ax = grp.plot(ax=axes[floor(i/2)][i%2], kind='line', x='timecum', y='acc', c='black', ls=lsd.get(key))
-        labels.append(key)
+    for i, k in enumerate(sorted(list(df_grp.groups.keys()))):
+        group = df_grp.get_group(k)
+        for key, grp in group.groupby(['instance_type']):
+            ax = grp.plot(ax=axes[floor(i/2)][i%2], kind='line', x=x, y='val_acc', c='black', ls=lsd.get(key))
+            labels.append(key)
+            if i == 0:
+                start, end = ax.get_xlim()
+                ax.xaxis.set_ticks(range(0, int(end)+1, 10000))
         lines, _ = ax.get_legend_handles_labels()
-        ax.legend(lines, labels, loc=4)
-        if i == 0:
-            start, end = ax.get_xlim()
-            ax.xaxis.set_ticks(range(0, int(end)+1, 10000))
-    axes[floor(i/2)][i%2].set_title('Zbiór danych: {}\nArchitektura: {}'.format(k.split('_')[0], k.split('_')[1]))
-    axes[floor(i/2)][i%2].set_xlabel('Czas obliczeń (s)')
-    axes[floor(i/2)][i%2].set_ylabel('Trafność')
+        ax.legend(lines, labels, loc='best')
+        axes[floor(i/2)][i%2].set_title('Zbiór danych: {}\nArchitektura: {}'.format(k.split('_')[0], k.split('_')[1]))
+        if x == 'timecum':
+            axes[floor(i/2)][i%2].set_xlabel('Czas obliczeń (s)')
+        elif x == 'price_ond':
+            axes[floor(i/2)][i%2].set_xlabel('Cena obliczeń on-demand ($)')
+        elif x == 'price_spot':
+            axes[floor(i/2)][i%2].set_xlabel('Cena obliczeń spot ($)')
+        axes[floor(i/2)][i%2].set_ylabel('Trafność')
 
-fig.suptitle('')
-fig.savefig('fig:experiment_acc.png')
+    fig.suptitle('')
+    fig.savefig('fig:experiment_acc_val_{}.png'.format(x))
 
-fig, axes = plt.subplots(nrows=3, ncols=2, figsize=[14, 18], sharey=False)
-fig.subplots_adjust(hspace=0.35,wspace=0.25)
-
-for i, k in enumerate(sorted(list(df_grp.groups.keys()))):
-    group = df_grp.get_group(k)
-    for key, grp in group.groupby(['instance_type']):
-        ax = grp.plot(ax=axes[floor(i/2)][i%2], kind='line', x='timecum', y='val_loss', c='black', ls=lsd.get(key))
-        labels.append(key)
-        if i == 0:
-            start, end = ax.get_xlim()
-            ax.xaxis.set_ticks(range(0, int(end)+1, 10000))
-    lines, _ = ax.get_legend_handles_labels()
-    ax.legend(lines, labels, loc='best')
-    axes[floor(i/2)][i%2].set_title('Zbiór danych: {}\nArchitektura: {}'.format(k.split('_')[0], k.split('_')[1]))
-    axes[floor(i/2)][i%2].set_xlabel('Czas obliczeń (s)')
-    axes[floor(i/2)][i%2].set_ylabel('Wartość funkcji celu')
-
-fig.suptitle('')
-fig.savefig('fig:experiment_loss_val.png')
-
-fig, axes = plt.subplots(nrows=3, ncols=2, figsize=[14, 18], sharey=False)
-fig.subplots_adjust(hspace=0.35,wspace=0.25)
-
-for i, k in enumerate(sorted(list(df_grp.groups.keys()))):
-    group = df_grp.get_group(k)
-    for key, grp in group.groupby(['instance_type']):
-        ax = grp.plot(ax=axes[floor(i/2)][i%2], kind='line', x='timecum', y='val_acc', c='black', ls=lsd.get(key))
-        labels.append(key)
-        if i == 0:
-            start, end = ax.get_xlim()
-            ax.xaxis.set_ticks(range(0, int(end)+1, 10000))
-    lines, _ = ax.get_legend_handles_labels()
-    ax.legend(lines, labels, loc='best')
-    axes[floor(i/2)][i%2].set_title('Zbiór danych: {}\nArchitektura: {}'.format(k.split('_')[0], k.split('_')[1]))
-    axes[floor(i/2)][i%2].set_xlabel('Czas obliczeń (s)')
-    axes[floor(i/2)][i%2].set_ylabel('Trafność')
-
-fig.suptitle('')
-fig.savefig('fig:experiment_acc_val.png')
+tabkeys = tmp.groupby(['dataset', 'architecture','instance_type']).quantile([0.1, 0.25, 0.5, 0.75, 0.9], interpolation='nearest')['acc'].reset_index()
+tabkeys = pd.merge(tabkeys, tmp, how='left', on=['dataset', 'architecture', 'instance_type', 'acc']).groupby(['dataset', 'architecture', 'instance_type', 'acc', 'level_3']).min()['index'].reset_index()
+tabr = pd.merge(tabkeys, tmp, how='inner', on=['dataset', 'architecture', 'instance_type', 'index'], suffixes=('_x',''))
+tab = tabr[['dataset', 'architecture', 'instance_type', 'level_3', 'acc', 'time', 'price_ond', 'price_spot']]
+valr = {'acc':4,'time':2,'price_ond':4, 'price_spot':4}
+print(tab.set_index(['dataset', 'architecture', 'instance_type', 'level_3']).round(valr).to_latex())
